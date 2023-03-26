280aafc8613c6314b77048b45cd29eba3cc6750f
surahj <odebodeopeyemi@gmail.com>
2023-03-26 17:08:40 +0100

    Create script for top five news

--- script.py
+++ script.py
@@ -2,19 +2,32 @@ import requests
 from bs4 import BeautifulSoup
 import pprint
 
+url = 'https://nairametrics.com/'
 
-news = requests.get('https://nairametrics.com/')
-news_soup = BeautifulSoup(news.text, 'html.parser')
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
 
-news_item_soup = news_soup.select('.jeg_post_title')
+news_item_soup = soup.select('.jeg_post_title')
 
-news_items = []
+#Top_five news
+print("Top five news")
+top_five_news = []
+top_stories = soup.find(class_='jeg_postblock_22').find_all(class_='jeg_post_title')
 
+for story in top_stories[:5]:
+    title = story.a.text.strip()
+    url = story.a['href']
+    top_five_news.append({'title': title, 'url': url})
+
+pprint.pprint(top_five_news)
+
+# All news
+print("All news")
+news = []
 for index, value in enumerate(news_item_soup):
   title = news_item_soup[index].a.getText()
   url = news_item_soup[index].a['href']
 
-  news_items.append({'title': title, 'url': url})
+  news.append({'title': title, 'url': url})
 
-print("news items ! below")
-pprint.pprint(news_items)
+pprint.pprint(news)
