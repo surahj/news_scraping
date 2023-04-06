89b1ccba1f58a5a3c162d932714b7e6f5214a9f4
surahj <odebodeopeyemi@gmail.com>
2023-04-06 09:39:01 +0100

    Create a script to scrape from google news

--- script.py
+++ script.py
@@ -1,33 +1,22 @@
-import requests
-from bs4 import BeautifulSoup
-import pprint
+from GoogleNews import GoogleNews
+import pandas as pd
 
-url = 'https://nairametrics.com/'
+googlenews = GoogleNews(lang='en')
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+googlenews.search('Business Nigeria')
 
-news_item_soup = soup.select('.jeg_post_title')
+#uncomment the line below to get more than 10 result
+#googlenews.getpage(2)
+# googlenews.getpage(3)
+result = googlenews.result()
+googlenews.get_texts()
+googlenews.set_period('12h')
 
-#Top_five news
-print("Top five news")
-top_five_news = []
-top_stories = soup.find(class_='jeg_postblock_22').find_all(class_='jeg_post_title')
 
-for story in top_stories[:5]:
-    title = story.a.text.strip()
-    url = story.a['href']
-    top_five_news.append({'title': title, 'url': url})
+for article in result:
+    for key, value in article.items():
+        print(key, ':', value)
 
-pprint.pprint(top_five_news)
+df = pd.DataFrame(result, columns=['title', 'link', 'date'])
 
-# All news
-print("All news")
-news = []
-for index, value in enumerate(news_item_soup):
-  title = news_item_soup[index].a.getText()
-  url = news_item_soup[index].a['href']
-
-  news.append({'title': title, 'url': url})
-
-pprint.pprint(news)
+df.to_csv('today_stories.csv', index=False)
